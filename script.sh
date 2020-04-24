0afb6428e43725ee5ef225aafc8efc054c120f8a
Piggey <49120531+Piggey@users.noreply.github.com>
2020-04-24 14:20:12 +0200

    Ubuntu 20.04 LTS

---
+++ script.sh
@@ -0,0 +1,12 @@
+sudo snap install chromium
+sudo snap install code --classic
+sudo snap install discord
+sudo snap install spotify
+sudo apt install -y python3
+sudo apt install -y python3-pip
+sudo add-apt-repository ppa:codeblocks-devs/release
+sudo apt-get update
+sudo apt-get install -y codeblocks codeblocks-contrib
+sudo snap install vlc
+sudo snap install libreoffice
+echo -------- downloading successful! --------
