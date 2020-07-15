6a6f3341acc15bf739e3765047557daafa3f02ca
Piggey <pepcio3320@gmail.com>
2020-07-15 11:32:19 +0200

    fixed snap, added steam and lutris

--- manjaro.sh
+++ manjaro.sh
@@ -1,15 +1,25 @@
 echo hi
 sudo pacman -S snapd --noconfirm
-sudo snap install chromium
-sudo snap install code --classic
-sudo snap install discord
+sudo systemctl enable --now snapd.socket
+sudo systemctl restart snapd.service
+
+sudo pacman -S chromium --noconfirm
+sudo pacman -R firefox --noconfirm
+
+sudo pacman -S code --noconfirm
+sudo pacman -S discord --noconfirm
 sudo snap install spotify
-sudo pacman -S python --noconfirm
+
+sudo pacman -S steam-native --noconfirm
+sudo pacman -S lutris --noconfirm
+
 sudo pacman -S codeblocks --noconfirm
-sudo snap install vlc
-sudo snap install libreoffice
+sudo pacman -S vlc --noconfirm
+sudo pacman -S libreoffice-fresh --noconfirm
 sudo pacman -S qbittorrent --noconfirm
+
 sudo pacman -S jre8-openjdk --noconfirm
 sudo pacman -S jdk8-openjdk --noconfirm
-sudo pacman -Syu
+
+sudo pacman -Syu --noconfirm
 echo bye
