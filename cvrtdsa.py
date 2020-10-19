2733bbd4a31f4d61b3c173704251aaaf61c6054e
Piggey <49120531+Piggey@users.noreply.github.com>
2020-10-19 15:07:23 +0200

    updated/added scripts

---
+++ cvrtdsa.py
@@ -0,0 +1,21 @@
+from sys import argv
+from PIL import Image
+
+outpath = "/home/piggey/Desktop/" + argv[1]
+imgs = argv[3:]
+
+print(f'converting image {argv[2]}')
+
+pdf = Image.open(argv[2])
+pdf = pdf.convert('RGB')
+print(f'appending images {imgs}\nsaving to {outpath}...')
+converted = []
+
+for image in imgs:
+    pil_img = Image.open(image)
+    print(f'converting {image}')
+    pil_img = pil_img.convert("RGB")
+    converted.append(pil_img)
+
+pdf.save(outpath, save_all=True, append_images=converted)
+print("done!")
