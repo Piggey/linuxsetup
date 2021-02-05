43bfe1d3819fa59cd56d795f44d3d0801cd39b64
Piggey <49120531+Piggey@users.noreply.github.com>
2021-02-05 21:16:36 +0100

    fixed verbosity

--- cvrt.py
+++ cvrt.py
@@ -6,17 +6,15 @@ home = getenv('HOME')
 outpath = f"{home}/Desktop/{argv[1]}"
 imgs = argv[3:]
 
-print(f'converting image {argv[2]}')
+print('converting images from /tmp/scanfiles folder...')
 pdf = Image.open(argv[2])
 pdf = pdf.convert('RGB')
 
-print(f'appending images {imgs}\nsaving to {outpath}...')
 converted = []
 for image in imgs:
     pil_img = Image.open(image)
-    print(f'converting {image}')
     pil_img = pil_img.convert("RGB")
     converted.append(pil_img)
 
 pdf.save(outpath, save_all=True, append_images=converted)
-print("done!")
+print('converted successfully!')
